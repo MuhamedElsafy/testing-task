8663f480ba7c04fd66565e4413300dd59d75d7ab
MuhamedElsafy <muhamedd.kh@gmail.com>
2024-02-11 21:53:05 +0200

    initial app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { CommitteeComponent } from './committee/committee.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SidebarComponent,
+    CommitteeComponent
   ],
   imports: [
     BrowserModule,
